1b6ae5c192a11dfad128836fbb1eed910b05539b
Aaron Harris <aamharris@gmail.com>
2018-08-13 23:36:56 -0500

    Add test for on pencil construction

---
+++ KataPencilDurability/KataPencilDurability.Tests/PencilTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KataPencilDurability.Domain;
+
+namespace KataPencilDurability.Tests
+{
+    [TestClass]
+    public class PencilTests
+    {
+        [TestMethod, TestCategory("Pencil Creation")]
+        public void Constructor_ValidAmounts_SetsPencilProperties()
+        {
+            var pencil = new Ticonderoga(10, 20, 30);
+            Assert.AreEqual(pencil.LetterCapacityPerSharpening, 10);
+            Assert.AreEqual(pencil.MaximumNumberOfSharpenings, 20);
+            Assert.AreEqual(pencil.EraserLetterCapacity, 30);
+        }
+    }
+}
