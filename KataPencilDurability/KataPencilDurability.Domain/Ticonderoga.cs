42905040d728bf2808d12f6e9fd8066c36c19937
Aaron Harris <aamharris@gmail.com>
2018-08-13 23:39:39 -0500

    Creating a ticonderoga pencil with properties added in constructor

---
+++ KataPencilDurability/KataPencilDurability.Domain/Ticonderoga.cs
@@ -0,0 +1,34 @@
+﻿using KataPencilDurability.Domain.Interfaces;
+using System;
+
+namespace KataPencilDurability.Domain
+{
+    public class Ticonderoga : IPencil, IEraser
+    {
+        public int LetterCapacityPerSharpening { get; set; }
+        public int MaximumNumberOfSharpenings { get; set; }
+        public int EraserLetterCapacity { get; set; }
+
+        public Ticonderoga(int letterCapacity, int maxNumberOfSharpenings, int eraserLetterCapacity)
+        {
+            this.LetterCapacityPerSharpening = letterCapacity;
+            this.MaximumNumberOfSharpenings = maxNumberOfSharpenings; 
+            this.EraserLetterCapacity = eraserLetterCapacity; 
+        }     
+
+        public void Sharpen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(char character)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Erase(string eraseText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
