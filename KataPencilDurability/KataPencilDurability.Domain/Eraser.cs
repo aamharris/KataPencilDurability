f9fca8a985614fc0c4aaa2b228369e0c7153eed2
Aaron Harris <aamharris@gmail.com>
2019-02-28 13:41:52 -0600

    add eraser logic

---
+++ KataPencilDurability/KataPencilDurability.Domain/Eraser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace KataPencilDurability.Domain
+{
+    public class Eraser
+    {
+        public int MaxEraserDegradation { get; private set; }
+        public int EraserDegradationRemaining { get; private set; }
+        public Paper Paper { get; set; }
+
+        public Eraser(Paper paper, int maxEraserDegradation)
+        {
+            Paper = paper;
+            MaxEraserDegradation = maxEraserDegradation;
+            EraserDegradationRemaining = MaxEraserDegradation; 
+        }
+
+        public void Erase(string textToErase)
+        {
+            if (Paper == null)
+            {
+                throw new Exception("No paper was provided. Can not erase without paper.");
+            }
+
+            if (EraserDegradationRemaining == 0)
+            {
+                throw new Exception("Eraser has been used up. Please buy a new pencil or eraser.");
+            }
+
+            string erasedText;
+            if (EraserDegradationRemaining < textToErase.Length)
+            {
+                int startSpacesIndex = textToErase.Length - EraserDegradationRemaining;
+                erasedText = textToErase.Remove(startSpacesIndex, EraserDegradationRemaining).Insert(startSpacesIndex, new string(' ', EraserDegradationRemaining));
+            }
+            else
+            {
+                erasedText = new string(' ', textToErase.Length);
+            }
+
+            Paper.LastEditedTextIndex = Regex.Match(Paper.Text, $"{textToErase}(?!.*{textToErase})").Index;
+            Paper.Text = Regex.Replace(Paper.Text, $"{textToErase}(?!.*{textToErase})", erasedText);
+            EraserDegradationRemaining -= erasedText.Length;
+        }
+    }
+}
